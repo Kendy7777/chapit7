d87464af14cbf92d066571dd751e18ccf9673871
Kendy7777 <141690216+Kendy7777@users.noreply.github.com>
2023-08-10 23:09:05 -0400

    Add files via upload

---
+++ multiplicationenoncechapit7.py
@@ -0,0 +1,16 @@
+tab=int(input("Tape yon valè silvouplè:"))
+print("Valèw tape a se:",tab)
+for x in range(1,11):
+    if (tab>0 and tab<=10):
+       
+        print(x, "X",tab,"=",x*tab)
+        
+    else:
+        print("ou tape yon move valè,eskew vle tape yon lot valè?")
+        break
+ 
+    
+    
+ 
+        
+            
